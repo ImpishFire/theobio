cab5c6eba2c86a6c6adda9f7fcf507909801eec5
Nirupama Sekar <89249913+ImpishFire@users.noreply.github.com>
2023-09-30 01:33:57 +0530

    add question

---
+++ main.py
@@ -0,0 +1,10 @@
+'''
+radius of gyration SARW #2
+complexity = 6
+Compute
+1. SARW trajectories 1, 2
+2. number of correct
+3. number of incorrect
+4. number of trap correct contacts - use complexity
+5. radius of gyration of SARW
+'''
